47463141ecf721ba97d611b0abaad90637c2dd38
Alex De Matos <alex.jrmts@gmail.com>
2022-02-10 16:07:26 -0300

    ending first three sections

---
+++ section_II-III/data_types.js
@@ -0,0 +1,40 @@
+//number type
+const num = 1;
+const str = "1";
+const bool = true;
+
+//math operations with num type
+console.log(2 * 6);
+console.log(2 - 6);
+console.log(2 + 6);
+console.log(2 / 6);
+console.log(20 % 6);
+console.log(21 % 6);
+
+//special nums
+console.log(Infinity);
+console.log(-Infinity);
+console.log(NaN);
+
+//string type
+console.log('teste 1');
+console.log("teste 2");
+console.log(`teste 3`);
+console.log(`teste 3 ` + 'teste 1');
+console.log(`teste\n4`);
+console.log("teste '5'");
+console.log(`teste ${3 + 3}`);
+
+//boolean type
+console.log(true);
+console.log(5 > 2);
+console.log(false);
+console.log(5 > 6);
+
+//logical operations
+console.log(1 === 1 && 2 === 2);
+console.log(1 === 2 || 2 === 2);
+
+//empty values
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ section_II-III/data_types_exercises.js
@@ -0,0 +1,5 @@
+console.log("teste " + 'teste ' + `teste`);
+console.log(1, 1.1, 2 + 3);
+console.log(1 > 0, 2 <= 1, 1 != 2);
+console.log(2 == 2 && 1 == 1, 3 != 3 || 1 != 1, !(2 === 3));
+console.log(20 * "aaa")
